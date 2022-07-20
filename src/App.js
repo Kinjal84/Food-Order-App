e1a89438ea97734e68f9c1ae36a3c08dc121425f
Kinjal84 <kinjalbosamiya621@gmail.com>
2022-07-20 12:05:51 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
+import { React, Fragment, useState } from 'react';
 import './App.css';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './components/Store/CartPovider';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [CartIsShown, setCartIsShown] = useState(false);
+
+    const ShowCartHandler = () => {
+        setCartIsShown(true);
+    };
+    const HideCartHandler = () => {
+        setCartIsShown(false);
+    };
+    return (
+        <CartProvider>
+            {CartIsShown && <Cart onClose={HideCartHandler} />}
+            <Header onShowCart={ShowCartHandler} />
+            <main>
+                <Meals />
+            </main>
+        </CartProvider>
+    );
 }
 
 export default App;
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,26 @@
+import classes from '../UI/Modal.module.scss';
+import { Fragment } from 'react';
+// import { ReactDOM } from 'react';
+
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <div className={classes.modal}>
+            <div className={classes.content}>{props.children}</div>
+        </div>
+    );
+};
+
+const Modal = (props) => {
+    return (
+        <Fragment>
+            <Backdrop onClose={props.onClose} />
+            <ModalOverlay>{props.children}</ModalOverlay>
+        </Fragment>
+    );
+};
+
+export default Modal;
